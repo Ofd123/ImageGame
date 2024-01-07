7226cd159b03316f2d88f669444e5c92649c4bc8
Cyber_User <ad6354@bs.amalnet.k12.il>
2024-01-07 14:06:15 +0200

    some changes

--- app/src/main/java/com/example/imagegame/MainActivity.java
+++ app/src/main/java/com/example/imagegame/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.imagegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,25 +21,25 @@ public class MainActivity extends AppCompatActivity
     int[] id = new int[3];
 
 
+    @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Iv[0]=(android.widget.ImageView) findViewById(R.id.androidNum1);
-        Iv[1]=(android.widget.ImageView) findViewById(R.id.pokemonNum2);
-        Iv[2]=(android.widget.ImageView) findViewById(R.id.minionsNum3);
+        Iv[0]=(ImageView) findViewById(R.id.androidNum1);
+        Iv[1]=(ImageView) findViewById(R.id.pokemonNum2);
+        Iv[2]=(ImageView) findViewById(R.id.minionsNum3);
         btn=findViewById(R.id.button);
-        id[0]=R.drawable.androidNum1;
-        id[1]=R.drawable.pokemonNum2;
-        id[2]=R.drawable.minionsNum3;
+        id[0]=R.drawable.androidone;
+        id[1]=R.drawable.pokemontwo;
+        id[2]=R.drawable.minionsthree;
     }
     public void clicked(View view)
     {
         justANumber=randomizer.nextInt(3);
         btn.setText("this is picture number: "+justANumber);
-        if (justANumber==1) {
-            Iv.setImageResource(R.drawable.androidNum1);
-        }
+        Iv[justANumber].setImageResource(id[justANumber]);
+
     }
 
 
