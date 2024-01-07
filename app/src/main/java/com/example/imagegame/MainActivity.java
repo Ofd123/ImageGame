b709d45ca292835a1d606f8beb756637f3379d0d
Cyber_User <ad6354@bs.amalnet.k12.il>
2024-01-07 13:21:38 +0200

    some changes

--- app/src/main/java/com/example/imagegame/MainActivity.java
+++ app/src/main/java/com/example/imagegame/MainActivity.java
@@ -3,33 +3,43 @@ package com.example.imagegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
 {
 
     //@Override
     Button btn;
-    ImageView num1;
-    ImageView num2;
-    ImageView num3;
-    ImageView Iv;
-
+    ImageView[] Iv = new ImageView[3];
+    public Random randomizer = new Random();
+    int justANumber;
+    int[] id = new int[3];
 
 
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        num1=findViewById(R.id.androidNum1);
-        num2=findViewById(R.id.pokemonNum2);
-        num3=findViewById(R.id.minionsNum3);
+        Iv[0]=(android.widget.ImageView) findViewById(R.id.androidNum1);
+        Iv[1]=(android.widget.ImageView) findViewById(R.id.pokemonNum2);
+        Iv[2]=(android.widget.ImageView) findViewById(R.id.minionsNum3);
         btn=findViewById(R.id.button);
-        Iv=findViewById(R.id.imageView);
+        id[0]=R.drawable.androidNum1;
+        id[1]=R.drawable.pokemonNum2;
+        id[2]=R.drawable.minionsNum3;
     }
     public void clicked(View view)
     {
+        justANumber=randomizer.nextInt(3);
+        btn.setText("this is picture number: "+justANumber);
+        if (justANumber==1) {
+            Iv.setImageResource(R.drawable.androidNum1);
+        }
+    }
 
 
 }
