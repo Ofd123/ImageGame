2f07e792ee8f49c5d908c50baa1d786e66aee122
Cyber_User <ad6354@bs.amalnet.k12.il>
2024-01-07 13:02:54 +0200

    added values

--- app/src/main/java/com/example/imagegame/MainActivity.java
+++ app/src/main/java/com/example/imagegame/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.imagegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    //@Override
+    Button btn;
+    ImageView num1;
+    ImageView num2;
+    ImageView num3;
+    ImageView Iv;
+
+
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        num1=findViewById(R.id.androidNum1);
+        num2=findViewById(R.id.pokemonNum2);
+        num3=findViewById(R.id.minionsNum3);
+        btn=findViewById(R.id.button);
+        Iv=findViewById(R.id.imageView);
     }
+    public void clicked(View view)
+    {
+
+
 }
